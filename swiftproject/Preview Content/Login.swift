c23cb893776677b936ab7dfe6d2c33bde8889b72
gopal <gopalpurvey708@gmail.com>
2025-07-11 15:40:21 +0530

    my project

--- swiftproject/Preview Content/Login.swift
+++ swiftproject/Preview Content/Login.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct Login: View {
     var body: some View {
         VStack{
-            Text("Users are asked to select their country from a list. This action automatically populates the country code in the subsequent phone number field. ")
+             Text("Users are asked to select their country from a list. This action automatically populates the country code in the subsequent phone number field. ")
         }
         
     }
